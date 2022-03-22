183434d0ce0a414973cae12deea59b9fa261343d
Daniel Nishimori <danielnishimoriboss@gmail.com>
2022-03-22 17:45:39 -0300

    simple components

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Character {
+    name: String,
+}
+
 fn main() {
     App::new()
+        // .add_plugins(DefaultPlugins)
+        .add_startup_system(startup_game)
         .add_system(msg)
         .run();
 }
 
-fn msg() {
-    println!("test with Bevy");
+fn startup_game(mut commands: Commands) {
+    commands.spawn().insert(Character {
+        name: "Daniel".to_string(),
+    });
+    commands.spawn().insert(Character {
+        name: "Yuliya".to_string(),
+    });
+    commands.spawn().insert(Character {
+        name: "Pozina".to_string(),
+    });
+}
+fn msg(query: Query<&Character>) {
+    for character in query.iter() {
+        println!("the {} exists", character.name);
+    }
 }
