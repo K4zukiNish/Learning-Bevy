b801cda03cf6b5ada34b1a68184205f940968776
Daniel Nishimori <danielnishimoriboss@gmail.com>
2022-03-22 19:51:21 -0300

    end bevy book

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,23 @@
-#![allow(dead_code)]
-
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Character {
-    name: String,
-    age: u32,
-}
-struct GreetTimer(Timer);
-
-fn startup_game(mut commands: Commands) {
-    commands.spawn().insert(Character {
-        name: "Daniel".to_string(),
-        age: 19,
-    });
-    commands.spawn().insert(Character {
-        name: "Yuliya".to_string(),
-        age: 22,
-    });
-    commands.spawn().insert(Character {
-        name: "Pozina".to_string(),
-        age: 12,
-    });
-}
-fn msg(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Character>) {
+struct MessageTimer(Timer);
+fn message(time: Res<Time>, mut timer: ResMut<MessageTimer>) {
     if timer.0.tick(time.delta()).just_finished() {
-        for character in query.iter() {
-            println!("the {} exists", character.name);
-        }
+        println!("A message");
     }
 }
 
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
+struct MainPlugin;
+impl Plugin for MainPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2., true)))
-            .add_startup_system(startup_game)
-            .add_system(msg);
+        app.insert_resource(MessageTimer(Timer::from_seconds(5., true)))
+            .add_system(message);
     }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(MainPlugin)
         .run();
 }
