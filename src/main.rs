ff119d28399c7d7397a0422f6551c3f5058230a1
NyshimoriMizuki@gmail.com <nyshimorikenji@gmail.com>
2023-04-16 15:47:18 -0300

    moving player

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 mod player;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::BLACK))
-        .insert_resource(WindowDescriptor {
-            ..Default::default()
-        })
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugin(player::PlayerPlugin)
+        .add_startup_system(spawn_camera)
         .run();
+}
+
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..Default::default()
+    });
 }
--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,72 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+const PLAYER_SPEED: f32 = 200.;
+
+pub struct PlayerPlugin;
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player).add_system(move_player);
+    }
+
+    fn setup(&self, _app: &mut App) {
+        // do nothing
+    }
+
+    fn name(&self) -> &str {
+        std::any::type_name::<Self>()
+    }
+
+    fn is_unique(&self) -> bool {
+        true
+    }
+}
 
 #[derive(Component)]
 pub struct Player;
+
+pub fn spawn_player(
+    mut command: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    assets: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    command.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: assets.load("sprites/ball_blue_large.png"),
+            ..Default::default()
+        },
+        Player {},
+    ));
+}
+
+fn move_player(
+    keyboard: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard.pressed(KeyCode::Left) || keyboard.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+        if keyboard.pressed(KeyCode::Right) || keyboard.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard.pressed(KeyCode::Up) || keyboard.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard.pressed(KeyCode::Down) || keyboard.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
